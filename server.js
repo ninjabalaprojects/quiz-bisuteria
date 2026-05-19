850c58c8fd4b3061574783cf6aee08da85738e20
ninjabalaprojects <ninjabalaprojects@github.com>
2026-05-19 14:17:37 -0300

    Initial commit - Quiz Bisuteria Lucrativa LATAM

---
+++ server.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3333;
+const DIR = __dirname;
+
+const mime = {
+  '.html': 'text/html',
+  '.css':  'text/css',
+  '.js':   'application/javascript',
+  '.png':  'image/png',
+  '.jpg':  'image/jpeg',
+  '.svg':  'image/svg+xml',
+};
+
+http.createServer((req, res) => {
+  let filePath = path.join(DIR, req.url === '/' ? 'index.html' : req.url);
+  const ext = path.extname(filePath);
+  fs.readFile(filePath, (err, data) => {
+    if (err) { res.writeHead(404); res.end('Not found'); return; }
+    res.writeHead(200, { 'Content-Type': mime[ext] || 'text/plain' });
+    res.end(data);
+  });
+}).listen(PORT, () => {
+  console.log('Server running at http://localhost:' + PORT);
+});
